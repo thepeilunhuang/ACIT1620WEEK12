fd59cdf50a52203ac1cab3bb3060f6cbb6375165
Peilun Huang <phuang61@my.bcit.ca>
2023-03-27 15:32:07 -0700

    use once true

--- script/script.js
+++ script/script.js
@@ -2,9 +2,9 @@ let buttonRef = document.querySelector('#clickMe');
 
 function alertUser(){
     alert('you clicked!');
-    buttonRef.removeEventListener('click', alertUser)
+    // buttonRef.removeEventListener('click', alertUser)
 }
 
-buttonRef.addEventListener('click', alertUser);
+buttonRef.addEventListener('click', alertUser, {once:true});
 
 
