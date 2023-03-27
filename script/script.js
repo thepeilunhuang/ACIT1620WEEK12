3f7a4f23e21255107ab950ca1bfcc90f35f645c6
Peilun Huang <phuang61@my.bcit.ca>
2023-03-27 15:26:38 -0700

    click alert

---
+++ script/script.js
@@ -0,0 +1,10 @@
+let buttonRef = document.querySelector('#clickMe');
+
+function alertUser(){
+    alert('you clicked!');
+    buttonRef.removeEventListener('click', alertUser)
+}
+
+buttonRef.addEventListener('click', alertUser);
+
+
